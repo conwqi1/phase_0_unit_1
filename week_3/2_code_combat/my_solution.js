0e87bd5abcebd262538ad66680d723e05f8330d7
conwqi1 <qinwuang@live.unc.edu>
2014-05-28 15:57:09 -0400

    Update my_solution.js

--- week_3/2_code_combat/my_solution.js
+++ week_3/2_code_combat/my_solution.js
@@ -1,12 +1,39 @@
-// I worked on this challenge [by myself, with:]
+// I worked on this challenge [by myself]
  
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
+ //1.rescue mission
+ // This is your code controlling Tharin!
+
+// this.moveDown();
+// this.moveDown();
+// this.moveRight();
+
+// this.moveUp();
+
+// // Edit it anywhere you like, add comments like these
+// this.moveUp();
+// this.moveRight();
+// this.moveRight();
+// this.moveDown();
+// this.attackNearbyEnemy();
  
  
- 
- 
+//  2.Grab the mushroom
+//  // Tharin might consider taking a detour after line 3.
+
+// this.moveUp();
+// this.moveRight();
+// this.moveUp();
+// this.moveLeft();
+
+// this.moveUp();
+
+// this.attackNearbyEnemy();
+
+3.Drink Me
+
  
  
  
