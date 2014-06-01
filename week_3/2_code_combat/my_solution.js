f3cf6a15d81457a1e0f38654320ca7eb77e504b4
conwqi1 <qinwuang@live.unc.edu>
2014-06-01 16:51:50 -0400

    Update my_solution.js

--- week_3/2_code_combat/my_solution.js
+++ week_3/2_code_combat/my_solution.js
@@ -3,15 +3,14 @@
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
+
+
  //1.rescue mission
  // This is your code controlling Tharin!
-
 // this.moveDown();
 // this.moveDown();
 // this.moveRight();
-
 // this.moveUp();
-
 // // Edit it anywhere you like, add comments like these
 // this.moveUp();
 // this.moveRight();
@@ -22,14 +21,11 @@
  
 //  2.Grab the mushroom
 //  // Tharin might consider taking a detour after line 3.
-
 // this.moveUp();
 // this.moveRight();
 // this.moveUp();
 // this.moveLeft();
-
 // this.moveUp();
-
 // this.attackNearbyEnemy();
 
 // 3.Drink Me
@@ -40,7 +36,130 @@
 // this.moveUp();
 // this.moveRight();
 // this.attackNearbyEnemy();
- 
+
+
+ //4. Taunt the Guards
+// right
+// bust
+// right
+// taunt
+// left
+// left
+// call attack
+// right
+// ask to follow
+// right
+// right
+// up
+// right
+// taunt
+// down
+// right
+// up
+// right
+// this.moveRight();
+// this.bustDownDoor();
+// this.moveRight();
+// this.say("Hey there!");
+// this.moveLeft();
+// this.moveLeft();
+// this.say("Attack!");
+// this.moveRight();
+// this.say("Follow me.");
+// this.moveRight();
+// this.moveRight();
+// this.moveUp();
+// this.moveRight();
+// this.say("Yo!");
+// this.moveDown();
+// this.moveRight();
+// this.moveUp();
+// this.moveRight();
+
+// 5.It's a Trap
+// down
+// down
+// taunt
+// up
+// up
+// this.moveDown();
+// this.moveDown();
+// this.say("What's up, Punk?!");
+// this.moveUp();
+// this.moveUp();
+
+// 6.Break the Prison
+// friend?
+// friend?
+// friend?
+// friend?
+// friend?
+// friend?
+// friend?
+// friend?
+// true
+// if(name === "William")
+//     return true;
+// if(name === "Krogg")
+//     return false;
+// if(name === "Lucas")
+//     return true;
+// if(name === "Brack")
+//     return false;
+// if(name === "Gort")
+//     return false;
+// if(name === "Robert")
+//     return true;
+// if(name === "Marcus")
+//     return true;
+// if(name === "MARCUS")
+//     return false;
+// return true;
+
+
+// 6.Taunt
+// taunt
+// taunt
+// taunt
+// taunt
+// this.say("Yo!");
+// this.say("You smell!");
+// this.say("Look at you!");
+// this.say("Ha!");
+
+// 7.Cowardly Taunt
+// up
+// taunt
+// down
+// this.moveXY(69, 30);
+// this.say("Sucker!");
+// this.moveXY(64, 11);
+
+// 8.Commanding Followers
+// right
+// ask to follow
+// down-left
+// ask to attack
+// this.moveXY(70, 65);
+// this.say("Follow me!");
+// this.moveXY(65, 40);
+// this.say("Attack!");
+
+// 9.Mobile Artillery
+// down-right
+// shoot down-right
+// up-right
+// shoot far up-right
+// shoot short up-right
+// shoot far up
+// shoot short up
+// this.moveXY(30, 26);
+// this.attackXY(46, 5);
+// this.moveXY(50, 39);
+// this.attackXY(71, 56);
+// this.attackXY(61, 46);
+// this.attackXY(46, 68);
+// this.attackXY(49, 51);
  
  
  
