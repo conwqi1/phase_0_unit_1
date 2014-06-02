f17527ec30c04e585adbfeac2db8a25349a0f40a
conwqi1 <qinwuang@live.unc.edu>
2014-06-02 01:21:11 -0400

    Update my_solution.js

--- week_3/5_design_cc_mission/my_solution.js
+++ week_3/5_design_cc_mission/my_solution.js
@@ -1,8 +1,8 @@
 // I worked [by myself, with:] on this challenge
 
 // Your mission description:
-// 
-// 
+// Have a monster named Jack, a gunner named Andy, and a girl named Mary. In oder to rescue the girl from the monster, 
+// the gunner have to kill the monster first and kiss the girl in order to succedd. 
 // 
 
 // Pseudocode
@@ -13,6 +13,26 @@
 // 
 
 // Initial Code
+var monster{
+  name:jack,
+  alive:true
+  }
+
+var gunner{
+  name:Andy,
+}
+var girl{
+  name:Mary,
+  kissed:false
+}
+
+function attack(){
+  monster.alive=false;
+}
+
+function kiss(){
+  girl.kissed=true;
+}
 
 
 
@@ -28,7 +48,7 @@
 
 // Reflection
 // 
-// 
+// I will revisit this challenge to make it better. I will need to get a more clear concept with this,and funciton generator.
 // 
 // 
 // 
